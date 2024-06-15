a77d9f9696f458192ddbbaaedcb1313a1bff97df
Abhner Araujo <abhner.araujo@asksuite.com>
2024-06-15 15:20:54 -0300

    feat: ssr

--- apps/coffee-calculator/src/app/app.config.ts
+++ apps/coffee-calculator/src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideClientHydration(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes), provideAnimationsAsync(),
+    provideRouter(appRoutes),
+    provideAnimationsAsync(),
   ],
 };
